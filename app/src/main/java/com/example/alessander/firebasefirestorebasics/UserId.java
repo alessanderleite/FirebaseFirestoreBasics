e27c4146c14b7d2ec79e0d911b776cb7b1872418
alessanderleite <alessander.logistica@gmail.com>
2018-11-02 12:17:45 -0300

    new class UserId

---
+++ app/src/main/java/com/example/alessander/firebasefirestorebasics/UserId.java
@@ -0,0 +1,13 @@
+package com.example.alessander.firebasefirestorebasics;
+
+import android.support.annotation.NonNull;
+
+public class UserId {
+
+    public String userId;
+
+    public <T extends UserId> T withId(@NonNull final String id) {
+        this.userId = id;
+        return (T) this;
+    }
+}
