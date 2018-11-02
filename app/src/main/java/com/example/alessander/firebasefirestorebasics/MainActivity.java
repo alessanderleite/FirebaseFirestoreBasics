c767c8eddff469f652e3cebe42ce6c0d5a9f6c5c
alessanderleite <alessander.logistica@gmail.com>
2018-11-01 23:36:17 -0300

    adding firestore into MainActivity

--- app/src/main/java/com/example/alessander/firebasefirestorebasics/MainActivity.java
+++ app/src/main/java/com/example/alessander/firebasefirestorebasics/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.alessander.firebasefirestorebasics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "FireLog";
+    private RecyclerView mMainList;
+    private FirebaseFirestore mFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainList = (RecyclerView) findViewById(R.id.main_list);
+        mFirestore = FirebaseFirestore.getInstance();
+
+        mFirestore.collection("Users").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot documentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                if (e != null) {
+                    Log.d(TAG, "Error : " + e.getMessage());
+                }
+
+                for (DocumentSnapshot doc : documentSnapshots) {
+
+                    String user_name = doc.getString("name");
+
+                    Log.d(TAG, "Name : " + user_name);
+                }
+            }
+        });
     }
 }
